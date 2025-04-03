da36fa18514ff3211efa91964d7fd2fb22da36ef
callcocam <callcocam@gmail.com>
2025-04-02 21:32:12 -0300

    refactor: Remove unused Category and Product controllers, requests, and resources

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('api')
     ->prefix('api/planogram')->name('planogram.api.')
-    ->group(function () {
-        Route::get('/products', [\Callcocam\Planogram\Http\Controllers\Api\ProductController::class, 'index'])->name('products.index');
-        Route::get('/categories', [\Callcocam\Planogram\Http\Controllers\Api\CategoryController::class, 'index'])->name('categories.index');
+    ->group(function () { 
         Route::resource('layers', \Callcocam\Planogram\Http\Controllers\Api\LayerController::class);
         Route::resource('segments', \Callcocam\Planogram\Http\Controllers\Api\SegmentController::class);
     });
